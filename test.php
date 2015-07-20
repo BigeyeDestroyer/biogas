34b01ce11d1346dca329e381d12de0932a7f15e1
ray <ray@raydeMacBook-Pro.local>
2015-07-20 11:18:06 +0800

    look up the php.ini

--- test.php
+++ test.php
@@ -5,6 +5,4 @@
  * Date: 7/14/15
  * Time: 4:20 PM
  */
-echo md5("ray");
-$sql="insert biogas_admin(username,password,email,phone) values('ray','070dd72385b8b2b205db53237da57200','lurui08@163.com','13439693669')";
-echo $sql;
+phpinfo();
